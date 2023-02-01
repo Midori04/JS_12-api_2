32d46ceb8e0016de742d36006d68f65f6e2d7282
Midori04 <machineko303@gmail.com>
2023-02-01 13:52:07 +0100

    first commit

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
 // const url ="https://api.openweathermap.org/data/2.5/weather?q=Røyken,no&units=metric&lang=no&appid=cda31770aeb83c15db6b9966ab82007d"
-var city = document.querySelector("#søk").value;
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},no&units=metric&lang=no&appid=cda31770aeb83c15db6b9966ab82007d`
 
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        document.querySelector("#btn").addEventListener("click", function () {           
-            
-            
-            document.querySelector(".by").innerHTML = data.name + ", " + data.sys.country;
+
+
+var btn = document.querySelector("#btn")
+btn.addEventListener("click", function () {
+    
+    var city = document.querySelector("#søk").value;
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=no&appid=cda31770aeb83c15db6b9966ab82007d`
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.name);
+
+            console.log(data.sys.country);
+
+            document.querySelector(".by").innerHTML = data.name +", "+ data.sys.country;
+
             document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + " °C";
-            document.querySelector(".beskrivelse").innerHTML = data.weather[0].description;
-            console.log(data.main.temp);
-        })
 
-    })
+            document.querySelector(".beskrivelse").innerHTML = data.weather[0].description;
+
+            document.querySelector(".min").innerHTML = data.main.temp_min
+
+            document.querySelector(".max").innerHTML = data.main.temp_max
+
+        })
+})
+
+// fetch(url)
+//     .then(response => response.json())
+//     .then(data => {
+//         document.querySelector("#btn").addEventListener("click", function () {           
+            
+            
+//             document.querySelector(".by").innerHTML = data.name + ", " + data.sys.country;
+//             document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + " °C";
+//             document.querySelector(".beskrivelse").innerHTML = data.weather[0].description;
+//             console.log(data.main.temp);
+//         })
+
+//     })
